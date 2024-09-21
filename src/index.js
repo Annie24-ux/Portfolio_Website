e84a65eaa013b8f1617d805b979029a62d97a39f
Siphesihle <sibiyasa24@gmail.com>
2024-09-21 17:43:58 +0200

    Added new design for an elegant look.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const pageSections= documents.querySelectorAll('section');
+const navLinks = documents.querySelectorAll('nav ul li a');
+
+
+window.addEventListener('scroll',() => {
+    let currentSection = '';
+
+    pageSections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if(payeYOffset >= sectionTop - 60){
+            currentSection = section.getAttribute('id');
+        }
+        
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').subString(1) === currentSection){
+            link.classList.add('active');
+        }
+
+
+    }
+    
+    );
+
+});
+
